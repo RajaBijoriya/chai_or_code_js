62c200ba06a92694630a79647153e75b56d76d33
RajaBijoriya <rajabijoriya875@gmail.com>
2025-09-18 21:28:05 +0530

    event ptojects

---
+++ AsyncCode/index.js
@@ -0,0 +1,15 @@
+
+// ye hai defoult js 
+// js is synchronous lang and single threaded 
+// jb js ko akele use (js engine ke sath ) krte hai tabhi hamko feel hota he ke thoda slow he 
+// but hum esko na ke barabr he single use krte hai baki hum ekso js engine ko kese,
+// kese na kese run time env ke sath he use krte hai 
+
+
+
+// execution context
+// syncronues code -> blocking code mtlb me kahu aap 5 min wait kru tb tk kuch mt 
+//                    krna me apke liye pani lata hu wait kro bus 
+
+// asyncrounuses code -> non-blocking code mtlb me bole 5 min wait kro app chahe toh 
+// apna phone use kr skte hai me jb tk pani lekr aata hu 
---
+++ Events/event.js
@@ -0,0 +1,1 @@
+// js me jetni bhi events hote hai voh sequensaly he run hote hai kuch exeption 
